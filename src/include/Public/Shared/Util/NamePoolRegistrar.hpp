b2982f238e4789659570df5092a344ad487f26b9
alphanin9 <47785076+alphanin9@users.noreply.github.com>
2024-11-16 06:14:41 +0200

    Added name pool registrar, now properly

---
+++ src/include/Public/Shared/Util/NamePoolRegistrar.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <RED4ext/RED4ext.hpp>
+#include <RedLib.hpp>
+
+namespace shared::raw
+{
+
+template<std::size_t N>
+struct StaticString
+{
+    char m_str[N];
+
+    constexpr StaticString(const char (&aStr)[N])
+    {
+        std::copy_n(aStr, N, m_str);
+    }
+};
+
+template<StaticString str>
+struct NamePoolRegistrar
+{
+    inline static auto s_registered = false;
+    static constexpr auto s_hash = Red::CName(&str.m_str[0]);
+
+    static Red::CName Get()
+    {
+        if (!s_registered)
+        {
+            Red::CNamePool::Add(&str.m_str[0]);
+            s_registered = true;
+        }
+
+        return s_hash;
+    }
+};
+} // namespace shared::raw
