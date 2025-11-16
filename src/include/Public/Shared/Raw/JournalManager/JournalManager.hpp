1a3e22f02c4e740afece36034f646e59c52c12e8
alphanin9 <47785076+alphanin9@users.noreply.github.com>
2025-11-16 06:55:23 +0200

    Steal more journal

--- src/include/Public/Shared/Raw/JournalManager/JournalManager.hpp
+++ src/include/Public/Shared/Raw/JournalManager/JournalManager.hpp
@@ -5,6 +5,7 @@
 #include <RedLib.hpp>
 
 #include <RED4ext/Scripting/Natives/Generated/game/JournalEntry.hpp>
+#include <RED4ext/Scripting/Natives/Generated/game/JournalEntryState.hpp>
 #include <RED4ext/Scripting/Natives/Generated/game/JournalManager.hpp>
 
 namespace shared::util::JournalManager
@@ -12,4 +13,7 @@ namespace shared::util::JournalManager
 constexpr auto GetEntryByHash =
     shared::util::RawVFunc<0x220, void* (Red::gameIJournalManager::*)(Red::Handle<Red::gameJournalEntry> & aOut,
                                                                       uint32_t aEntryHash)>();
-}
+
+constexpr auto GetEntryState = shared::util::RawVFunc<0x2B0, Red::gameJournalEntryState (Red::gameIJournalManager::*)(
+                                                                 Red::Handle<Red::gameJournalEntry>& aEntry)>();
+} // namespace shared::util::JournalManager
