19193af9aa3893055e740447ae66536e627d917e
alphanin9 <47785076+alphanin9@users.noreply.github.com>
2026-04-08 20:01:20 +0300

    Fix for 1.0.0 without container update

--- src/include/Public/Shared/Raw/String/String.hpp
+++ src/include/Public/Shared/Raw/String/String.hpp
@@ -70,7 +70,7 @@ struct UTF16String
         Red::DynArray<char> buffer;
         
         buffer.Reserve(utf8Length + 1);
-        buffer.Resize(utf8Length);
+        buffer.size = utf8Length;
 
         ::WideCharToMultiByte(CP_UTF8, 0, data, static_cast<int>(count), buffer.Data(), utf8Length, nullptr, nullptr);
 
